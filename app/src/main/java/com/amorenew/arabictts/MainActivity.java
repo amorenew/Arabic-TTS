03bd390428000d10a8da5f561231afb239076140
amorenew <amorenew@gmail.com>
2017-02-12 13:50:47 +0300

    Merge branch 'master' of https://github.com/amorenew/Arabic-TTS
    # Conflicts:
    #	.gitignore

--- app/src/main/java/com/amorenew/arabictts/MainActivity.java
+++ app/src/main/java/com/amorenew/arabictts/MainActivity.java
@@ -1,13 +1,40 @@
 package com.amorenew.arabictts;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playAudio();
+    }
+
+    private void playAudio() {
+        try {
+            AssetFileDescriptor descriptor = getAssets().openFd("test2.wav");
+            player = new MediaPlayer();
+            player.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+            descriptor.close();
+            player.prepare();
+            player.start();
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+            player.setLooping(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
